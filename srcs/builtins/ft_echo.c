de1bca721e08da4041754b5f44b952fb06ed2ad6
Mohamed Rital <mrital-@c1r4p2.1337.ma>
2023-10-19 18:51:12 +0100

    push expand

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -6,47 +6,96 @@
 /*   By: mrital- <mrital-@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:44:29 by mbelouar          #+#    #+#             */
-/*   Updated: 2023/10/19 16:12:42 by mrital-          ###   ########.fr       */
+/*   Updated: 2023/10/19 18:48:12 by mrital-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-static void	fill_new_str(char *s, char *res, int *i, int *j)
-{
-	while (s[*i])
-	{
-		if (s[*i] != 34 && s[*i] != 39)
-		{
-			res[*j] = s[*i];
-			(*j)++;
-		}
-		(*i)++;
-	}
+#include <stdio.h>
+#include <stdlib.h>
+
+static void fill_new_str(char *s, char *res, int *i, int *j) {
+    int inside_quotes = 0;
+    char quote = '\0';
+
+    while (s[*i]) {
+        if (inside_quotes) {
+            if (s[*i] == quote) {
+                inside_quotes = 0;
+                quote = '\0';
+            } else {
+                res[(*j)++] = s[*i];
+            }
+        } else {
+            if (s[*i] == '"' || s[*i] == '\'') {
+                inside_quotes = 1;
+                quote = s[*i];
+            } else {
+                res[(*j)++] = s[*i];
+            }
+        }
+        (*i)++;
+    }
 }
 
-char	*ft_remove_quotes(char *s)
-{
-	char	*res;
-	int		len;
-	int		i;
-	int		j;
+char *ft_remove_quotes(char *s) {
+    char *res;
+    int len;
+    int i;
+    int j;
 
-	i = 0;
-	len = 0;
-	while (s[i])
-	{
-		if (s[i] != 34 && s[i] != 39)
-			len++;
-		i++;
-	}
-	res = ft_calloc(len, sizeof(char));
-	i = 0;
-	j = 0;
-	fill_new_str(s, res, &i, &j);
-	res[j] = '\0';
-	return (res);
+    i = 0;
+    len = 0;
+    while (s[i]) {
+        if (s[i] != '"' && s[i] != '\'') {
+            len++;
+        }
+        i++;
+    }
+    res = (char *)malloc((len + 1) * sizeof(char));
+    i = 0;
+    j = 0;
+    fill_new_str(s, res, &i, &j);
+    res[j] = '\0';
+    return res;
 }
+
+// static void	fill_new_str(char *s, char *res, int *i, int *j)
+// {
+// 	while (s[*i])
+// 	{
+// 		if (s[*i] != 34 && s[*i] != 39)
+// 		{
+// 			res[*j] = s[*i];
+// 			(*j)++;
+// 		}
+// 		(*i)++;
+// 	}
+// }
+
+// char	*ft_remove_quotes(char *s)
+// {
+// 	char	*res;
+// 	int		len;
+// 	int		i;
+// 	int		j;
+
+// 	i = 0;
+// 	len = 0;
+// 	while (s[i])
+// 	{
+// 		if (s[i] != 34 && s[i] != 39)
+// 			len++;
+// 		i++;
+// 	}
+// 	res = ft_calloc(len, sizeof(char));
+// 	i = 0;
+// 	j = 0;
+// 	fill_new_str(s, res, &i, &j);
+// 	res[j] = '\0';
+// 	return (res);
+// }
 
 void	ft_echo(char **args)
 {
